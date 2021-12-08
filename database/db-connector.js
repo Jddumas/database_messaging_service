dda40595ad58f084ca134968ac0dffb95edc0b0e
Jddumas <67508827+jacobdmdumas@gmail.com>
2021-12-08 11:45:52 -0600

    update db-connector

--- database/db-connector.js
+++ database/db-connector.js
@@ -4,10 +4,10 @@ var mysql = require('mysql')
 // Create a 'connection pool' using the provided credentials
 var pool = mysql.createPool({
     connectionLimit : 10,
-    host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_dumasja',
-    password        : '1078',
-    database        : 'cs340_dumasja'
+    host            : 'us-cdbr-east-04.cleardb.com',
+    user            : 'b0124278ed988f',
+    password        : 'd9f45ea9',
+    database        : 'heroku_6b26380cebe4a26'
 })
 
 // Export it for use in our applicaiton
